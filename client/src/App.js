32131d0bdc5ed6bf8d9d5052bc1e9cdd59f368b7
Arkadiusz Zygan <a.zygan@gmail.com>
2021-10-04 11:32:55 +0200

    Prepare front

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,14 @@ import "./App.css";
 function App() {
   return (
     <div className='App'>
-      <h1>start</h1>
+      <h1>CRUD APP</h1>
+      <div className='form'>
+        <label htmlFor=''>Movie Name:</label>
+        <input type='text' name='movieName' />
+        <label htmlFor=''>Review:</label>
+        <input type='text' name='review' />
+      </div>
+      <button>Submit</button>
     </div>
   );
 }
